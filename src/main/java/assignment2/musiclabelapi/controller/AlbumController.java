d3568194bad22980075fb913ddae30bc55709a02
eskilschjong <eskil.schjong@hotmail.no>
2026-01-20 15:41:19 +0100

    feat: added album controller get methods for all and by id

--- src/main/java/assignment2/musiclabelapi/controller/AlbumController.java
+++ src/main/java/assignment2/musiclabelapi/controller/AlbumController.java
@@ -2,7 +2,9 @@ package assignment2.musiclabelapi.controller;
 
 import assignment2.musiclabelapi.model.Album;
 import assignment2.musiclabelapi.service.AlbumService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +18,18 @@ public class AlbumController {
         this.albumService = albumService;
     }
 
-    @GetMapping("/albums")
-    public List<Album> getAllAlbums() {
+    @GetMapping("/album/all")
+    public ResponseEntity<List<Album>> getAllAlbums() {
         var albums = albumService.getAllAlbums();
-        return albums;
+        return ResponseEntity.ok(albums);
+    }
+
+    @GetMapping("/album/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable(required = false) Long id) {
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var album = albumService.getAlbumById(id);
+        return ResponseEntity.ok(album);
     }
 }
