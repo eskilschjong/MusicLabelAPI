8a23853bd1fd2e791f744bac4a760c50edf48e31
eskilschjong <eskil.schjong@hotmail.no>
2026-01-20 13:27:51 +0100

    feat: added crud methods for album service

---
+++ src/main/java/assignment2/musiclabelapi/service/AlbumService.java
@@ -0,0 +1,32 @@
+package assignment2.musiclabelapi.service;
+
+import assignment2.musiclabelapi.model.Album;
+import assignment2.musiclabelapi.repository.AlbumRepositoryJDBC;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlbumService {
+    
+    private final AlbumRepositoryJDBC albumRepositoryJDBC;
+
+    public AlbumService(AlbumRepositoryJDBC albumRepositoryJDBC) {
+        this.albumRepositoryJDBC = albumRepositoryJDBC;
+    }
+
+    public Album createAlbum(Album album) {
+        return albumRepositoryJDBC.createAlbum(album);
+    }
+
+    public Album getAlbumById(long id) {
+        return albumRepositoryJDBC.getAlbumById(id);
+    }
+
+    public Album updateAlbum(Album album) {
+        return albumRepositoryJDBC.updateAlbum(album);
+    }
+
+    public boolean deleteAlbumById(long id) {
+        return albumRepositoryJDBC.deleteAlbumById(id);
+    }
+
+}
