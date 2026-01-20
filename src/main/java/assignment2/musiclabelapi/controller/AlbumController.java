d2d471385e0de42102143ea490e70ecee2e54ebd
eskilschjong <eskil.schjong@hotmail.no>
2026-01-20 15:31:23 +0100

    refactor: updated album to use hibernate

--- src/main/java/assignment2/musiclabelapi/controller/AlbumController.java
+++ src/main/java/assignment2/musiclabelapi/controller/AlbumController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 public class AlbumController {
 
-    private AlbumService albumService;
+    private final AlbumService albumService;
 
     public AlbumController(AlbumService albumService) {
         this.albumService = albumService;
