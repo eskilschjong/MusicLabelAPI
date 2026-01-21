f0919281d6878b1847a738e6b3c01a526a88c90b
eskilschjong <eskil.schjong@hotmail.no>
2026-01-21 14:50:36 +0100

    feat: added artist repository

---
+++ src/main/java/assignment2/musiclabelapi/repository/ArtistRepository.java
@@ -0,0 +1,9 @@
+package assignment2.musiclabelapi.repository;
+
+import assignment2.musiclabelapi.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+}
