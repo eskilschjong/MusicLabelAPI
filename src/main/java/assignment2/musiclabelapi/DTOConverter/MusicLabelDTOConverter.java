6c12a5d6406a4e7965039baeb226ffef01b53e33
eskilschjong <eskil.schjong@hotmail.no>
2026-01-23 15:49:12 +0100

    feat: added MusicLabelDTOConverter and MusicLabelEntityConverter

---
+++ src/main/java/assignment2/musiclabelapi/DTOConverter/MusicLabelDTOConverter.java
@@ -0,0 +1,20 @@
+package assignment2.musiclabelapi.DTOConverter;
+
+import assignment2.musiclabelapi.DTO.MusicLabelReadDTO;
+import assignment2.musiclabelapi.model.Album;
+import assignment2.musiclabelapi.model.MusicLabel;
+
+public class MusicLabelDTOConverter {
+
+    public MusicLabelReadDTO convertToDTO(MusicLabel musicLabel) {
+
+        MusicLabelReadDTO musicLabelReadDTO = new MusicLabelReadDTO();
+
+        musicLabelReadDTO.setId(musicLabel.getId());
+        musicLabelReadDTO.setName(musicLabel.getName());
+        musicLabelReadDTO.setDescription(musicLabel.getDescription());
+        musicLabelReadDTO.setAlbumIds(musicLabel.getAlbums().stream().map(Album::getId).toList());
+
+        return musicLabelReadDTO;
+    }
+}
