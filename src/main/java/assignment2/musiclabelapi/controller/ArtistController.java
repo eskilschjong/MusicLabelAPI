cee496a671faeb0f78fee34d67d78bc7e1554b12
eskilschjong <eskil.schjong@hotmail.no>
2026-01-23 15:13:10 +0100

    refactor: changed artist controller endpoints to use DTOs

--- src/main/java/assignment2/musiclabelapi/controller/ArtistController.java
+++ src/main/java/assignment2/musiclabelapi/controller/ArtistController.java
@@ -1,48 +1,67 @@
 package assignment2.musiclabelapi.controller;
 
 
+import assignment2.musiclabelapi.DTOConverter.ArtistDTOConverter;
+import assignment2.musiclabelapi.DTOConverter.ArtistEntityConverter;
+import assignment2.musiclabelapi.DTO.ArtistReadDTO;
+import assignment2.musiclabelapi.DTO.ArtistWriteDTO;
 import assignment2.musiclabelapi.model.Artist;
+import assignment2.musiclabelapi.service.AlbumService;
 import assignment2.musiclabelapi.service.ArtistService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/artist")
 public class ArtistController {
 
     private final ArtistService artistService;
+    private final ArtistDTOConverter converter;
+    private final ArtistEntityConverter entityConverter;
 
-    public ArtistController(ArtistService artistService) {
+    public ArtistController(ArtistService artistService, ArtistDTOConverter converter, ArtistEntityConverter entityConverter) {
         this.artistService = artistService;
+        this.converter = converter;
+        this.entityConverter = entityConverter;
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Artist>> getAllArtists() {
-    var artists = artistService.getAllArtists();
-    return ResponseEntity.ok(artists);
+    public ResponseEntity<List<ArtistReadDTO>> getAllArtists() {
+        var artists = artistService.getAllArtists();
+        var artistDTOs = artists.stream()
+                .map(converter::convertToDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(artistDTOs);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Artist> getArtistById(@PathVariable(required = false) Long id) {
+    public ResponseEntity<ArtistReadDTO> getArtistById(@PathVariable(required = false) Long id) {
         if (id == null) {
             return ResponseEntity.badRequest().build();
         }
         var artist = artistService.getArtistById(id);
-        return ResponseEntity.ok(artist);
+        var artistDTO = converter.convertToDTO(artist);
+        return ResponseEntity.ok(artistDTO);
     }
 
     @PostMapping
-    public ResponseEntity<Artist> createArtist(@RequestBody Artist artist) {
+    public ResponseEntity<ArtistReadDTO> createArtist(@RequestBody ArtistWriteDTO artistWriteDTO) {
+        Artist artist = entityConverter.convertToEntity(artistWriteDTO);
         Artist created = artistService.createArtist(artist);
-        return ResponseEntity.status(201).body(created);
+        ArtistReadDTO artistReadDTO = converter.convertToDTO(created);
+        return ResponseEntity.status(HttpStatus.CREATED).body(artistReadDTO);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Artist> updateArtist(@PathVariable Long id, @RequestBody Artist artist) {
+    public ResponseEntity<ArtistReadDTO> updateArtist(@PathVariable Long id, @RequestBody ArtistWriteDTO artistWriteDTO) {
+        Artist artist = entityConverter.convertToEntity(artistWriteDTO);
         Artist updated = artistService.updateArtist(id, artist);
-        return ResponseEntity.ok(updated);
+        ArtistReadDTO artistReadDTO = converter.convertToDTO(updated);
+        return ResponseEntity.ok(artistReadDTO);
     }
 
     @DeleteMapping("/{id}")
