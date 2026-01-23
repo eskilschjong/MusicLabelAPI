884d3f4e667232cf483c8e184c6f3c65c7eb154b
eskilschjong <eskil.schjong@hotmail.no>
2026-01-23 14:52:12 +0100

    feat: added ArtistReadDTO and ArtistWriteDTO

---
+++ src/main/java/assignment2/musiclabelapi/DTO/ArtistReadDTO.java
@@ -0,0 +1,61 @@
+package assignment2.musiclabelapi.DTO;
+
+import java.util.List;
+
+public class ArtistReadDTO {
+
+    private Long id;
+    private String name;
+    private String stageName;
+    private String coverImage;
+    private String biography;
+    private List<Long> AlbumIds;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Long> getAlbumIds() {
+        return AlbumIds;
+    }
+
+    public void setAlbumIds(List<Long> albumIds) {
+        AlbumIds = albumIds;
+    }
+}
